452e31da21f88f1f17300683e81d0b127717dbbe
Vipul <25aima61@kristujayanti.com>
2026-04-01 00:11:18 +0530

    fixed syntax error

--- backend/server.js
+++ backend/server.js
@@ -60,5 +60,5 @@ app.get("/orders", async (req, res) => {
 const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
-  console.log(Server running on port ${PORT});
+  console.log('Server running on port${PORT}');
 });
